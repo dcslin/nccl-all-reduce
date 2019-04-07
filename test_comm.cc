6f13ee205603cc3cf833b88a7c2724f8174edf82
shicong <shicong@comp.nus.edu.sg>
2019-04-07 19:24:21 +0800

    m

--- test_comm.cc
+++ test_comm.cc
@@ -1,5 +1,22 @@
 #include "communicator.h"
 
+void test(){
+  int size=4;
+  float send[size] = {1.0,2.0,1.0,2.0};
+  float* send_d;
+
+  CUDACHECK(cudaSetDevice(0));
+  CUDACHECK(cudaMalloc((void **)&send_d, size*sizeof(float)));
+  CUDACHECK(cudaMemcpy(send_d,send,size*sizeof(float),cudaMemcpyHostToDevice));
+
+  float receive[size];
+  CUDACHECK(cudaMemcpy(receive,send_d,size*sizeof(float),cudaMemcpyDeviceToHost));
+
+  std::cout << "received" << receive[0] << "\n";
+
+  cudaFree(&send_d);
+}
+
 int main(int argc, char *argv[])
 {
   // init MPI env
@@ -12,22 +29,37 @@ int main(int argc, char *argv[])
 
 
   // alloc mem assume this is done before hand
+  // nDev pointers to cudaMem
   float** sendbuff = (float**)malloc(nDev * sizeof(float*));
   float** recvbuff = (float**)malloc(nDev * sizeof(float*));
+
+  float send[size];
+  for(int i=0; i<size; i++)
+    send[i]=i+10.0;
 
   for (int i=0; i<nDev; i++) {
     CUDACHECK(cudaSetDevice(c.MPIRankInLocal*nDev + i));
     CUDACHECK(cudaMalloc(sendbuff + i, size * sizeof(float)));
     CUDACHECK(cudaMalloc(recvbuff + i, size * sizeof(float)));
-    CUDACHECK(cudaMemset(sendbuff[i], 1, size * sizeof(float)));
+
+    CUDACHECK(cudaMemcpy(sendbuff[i],send,size*sizeof(float),cudaMemcpyHostToDevice));
     CUDACHECK(cudaMemset(recvbuff[i], 0, size * sizeof(float)));
   }
 
+  //test();
+
   // main process of all reduce
-  std::cout<<"doning all reduce..\n";
+  std::cout<<"doing all reduce..\n";
   c.allReduce(size, sendbuff, recvbuff);
-  std::cout<<"doning waiting..\n";
+  std::cout<<"doing waiting..\n";
   c.wait();
+
+  //just test for first gpu
+  float receive[size];
+  CUDACHECK(cudaSetDevice(0));
+  CUDACHECK(cudaMemcpy(receive,recvbuff[0],size*sizeof(float),cudaMemcpyDeviceToHost));
+  std::cout << "received buff: " << receive[0] << " vs 40 expected"<< "\n";
+  std::cout << "received buff: " << receive[1] << " vs 44 expected"<< "\n";
 
   // clean up
   for (int i=0; i<nDev; i++) {
